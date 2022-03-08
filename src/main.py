84d839727190192f0bfc04d0b72ff31fc60ef55e
Lim <bigmanmin1@naver.com>
2022-03-08 18:15:14 +0900

    update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from pygame.locals import *
 pygame.init() 
 
 while True:
-	for event in pygame.event.get():		if event.type == QUIT:
+	for event in pygame.event.get():
+	  if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
 	pygame.display.update()
