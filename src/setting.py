532d9418e4a885f37df82156baf61c434e754ec3
Lim <bigmanmin1@naver.com>
2022-03-09 23:45:30 +0900

    update setting.py

--- src/setting.py
+++ src/setting.py
@@ -1,22 +1,26 @@
 import json
 
-settingData = {
-    "1.FirstName": "Gildong",
-    "2.LastName": "Hong",
-    "3.Age": 20, 
-    "4.University": "Yonsei University",
-    "5.Courses": [
-        {
-            "Major": "Statistics",
-            "Classes": ["Probability", 
-                        "Generalized Linear Model", 
-                        "Categorical Data Analysis"]
-        }, 
-        {
-            "Minor": "ComputerScience", 
-            "Classes": ["Data Structure", 
-                        "Programming", 
-                        "Algorithms"]
-        }
-    ]
+student_data = {
+  "1.FirstName": "Gildong",
+  "2.LastName": "Hong",
+  "3.Age": 20, 
+  "4.University": "Yonsei University",
+  "5.Courses": [
+    {
+      "Major": "Statistics",
+      "Classes": [
+        "Probability", 
+        "Generalized Linear Model", 
+        "Categorical Data Analysis"
+      ]
+    }, 
+    {
+      "Minor": "ComputerScience", 
+      "Classes": [
+        "Data Structure", 
+        "Programming", 
+        "Algorithms"
+      ]
+    }
+  ]
 } 
