ef9a0c28fc3fd6236b347fc1966a50d92048571b
Lim <bigmanmin1@naver.com>
2022-03-10 08:59:56 +0900

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import sys, pygame
 import screen_setting as sc
+from tkinter import *
 from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode(400,100)
+screen = pygame.display.set_mode((100,100))
 clock = pygame.time.Clock(sc.data["clock"]) 
 
 while True:
@@ -12,4 +13,3 @@ while True:
 	  	pygame.quit()
 	  	sys.exit()
 	pygame.display.update()
-  
