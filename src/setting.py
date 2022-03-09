8cd43bf8c3ff2d9db6de543a07eef8a84878f98c
Lim <bigmanmin1@naver.com>
2022-03-10 08:36:44 +0900

    update setting.py

--- src/setting.py
+++ src/setting.py
@@ -1,29 +1,20 @@
 import json
+from tkinter import *
 
-student_data = {
-  "1.FirstName": "Gildong",
-  "2.LastName": "Hong",
-  "3.Age": 20, 
-  "4.University": "Yonsei University",
-  "5.Courses": [
-    {
-      "Major": "Statistics",
-      "Classes": [
-        "Probability", 
-        "Generalized Linear Model", 
-        "Categorical Data Analysis"
-      ]
-    }, 
-    {
-      "Minor": "ComputerScience", 
-      "Classes": [
-        "Data Structure", 
-        "Programming", 
-        "Algorithms"
-      ]
-    }
+root = Tk()
+monitor_height = root.winfo_screenheight()
+monitor_width = root.winfo_screenwidth()
+print("width x height = %d x %d (pixels)" %(monitor_width, monitor_height))
+
+data = {
+  "size": [
+    monitor_height,
+    monitor_width
+  ],
+  "shader": "True"
   ]
 }
-
-with open("student_file.json", "w") as json_file:
-  json.dump(student_data, json_file)
+mainloop()
+with open("setting_data.json", "w") as json_file:
+	json.dump(data, json_file)
+	
