fa4c1dcd14beb1670cf24635c1866da0edacf979
Lim <bigmanmin1@naver.com>
2022-03-10 08:36:16 +0900

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-import sys 
-import pygame
+import sys, pygame
+import screen_setting as sc
 from pygame.locals import *
-from . import *
 
 pygame.init()
-screen = pygame.display.set_mode((0,0))
-clock = pygame.time.Clock() 
+screen = pygame.display.set_mode(400,100)
+clock = pygame.time.Clock(sc.data["clock"]) 
 
 while True:
 	for event in pygame.event.get():
