f045c3b76265ea9bbfa01cf36e3c87d8927baf18
Lim <68288682+SungMin59@users.noreply.github.com>
2022-03-10 16:45:00 +0000

    commit1-1

--- src/setting.py
+++ src/setting.py
@@ -1,29 +1,34 @@
-import json
+import json, pygame
 from tkinter import *
 
-class screen:
-  def getSetting(a,b):
-    with open("./data/setting_data.json", "r") as json_file:
-      
-    
-  def addSetting(a,b):
-    with open("./data/setting_data.json", "w") as json_file:
-      return json.dump(data, json_file) 
-    
 '''
-root = Tk()
-monitor_height = root.winfo_screenheight()
-monitor_width = root.winfo_screenwidth()
-print("width x height = %d x %d (pixels)" %(monitor_width, monitor_height))
-
 data = {
   "size": [
     monitor_height,
     monitor_width
   ],
-  "shader": "True"
-  ]
+  "shader": True
 }
-mainloop()
 '''
+class Screen:
+  
+  def __init__(self):
+  	self.root = Tk()
+  	self.monitor_size = [self.root.winfo_screenheight(), self.root.winfo_screenwidth()]
+  	self.shader = True
+  	self.data = {
+  	  "size": self.monitor_size,
+        "shader": True
+      } 
+  	
+  def importSetting(self):
+    with open("./data/setting_data.json", "r") as fs:
+      return fs
+    
+  def exportOption(self):
+    with open("./data/setting_data.json", "w") as fs:
+      return json.dump(self.data, fs)
+
+root = Tk()
+mainloop()
 	
